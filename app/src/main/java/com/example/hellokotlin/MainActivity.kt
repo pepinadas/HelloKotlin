450b904206eca49387db9d97d7148045eab33ff7
pepinadas <mmmunapregunta@gmail.com>
2021-05-01 17:29:14 -0500

    Primer Commit, del primer proyecto en Android Studio, primera seccion Terminada

---
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    var tts: TextToSpeech? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this, this)
+
+        findViewById<Button>(R.id.btnPlay).setOnClickListener{speak()}
+
+    }
+
+    private fun speak(){
+        var message: String = findViewById<TextView>(R.id.etMessage).text.toString()
+
+
+        if (message.isEmpty()){
+            findViewById<TextView>(R.id.tvStatus).text = "Introducir un texto"
+            message = "¿Es en serio? Ya pon algo en el edit Text!"
+        }
+
+
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+
+    }
+
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.tvStatus).text = "Listo!"
+            tts!!.setLanguage(Locale("ES"))
+
+        } else{
+            findViewById<TextView>(R.id.tvStatus).text = "No disponible :c"
+
+        }
+
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+}
